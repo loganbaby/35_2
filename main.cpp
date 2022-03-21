7dccd59aac828756c96811c2e9ff442dfd60c869
logbaby <96885996+loganbaby@users.noreply.github.com>
2022-03-21 16:30:03 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+int main() {
+  auto getRetryingCount = [](std::vector<int>& vec, int& number) {
+    std::unordered_set<int> set(vec.begin(), vec.end());
+    return set.count(number);
+  };
+
+  std::vector<int> vec =
+      std::initializer_list<int>{1, 2, 2, 2, 3, 4, 5, 4, 5, 4, 5};
+
+  std::cout << "Enter the number:" << std::endl;
+  std::cout << ">>> ";
+  int number = 0;
+  std::cin >> number;
+  std::cout << getRetryingCount(vec, number) << std::endl;
+
+  auto getRetryingSet = [](std::vector<int>& vec) {
+    std::unordered_set<int> set(vec.begin(), vec.end());
+    
+    auto vec_2 = std::make_unique<std::unordered_set<int>>(set);
+    return vec_2;
+  };
+
+  auto vec_2 = std::make_unique<std::unordered_set<int>>();
+  vec_2 = getRetryingSet(vec);
+
+  for (auto& i : *vec_2) {
+    std::cout << i << " ";
+  }
+}
